5196ff04efa7a68743b580d900ccda4d838c5ded
Stanislav Savuliak <stassavulak@gmail.com>
2025-11-08 20:21:53 +0100

    Initial commit

---
+++ src/components/layout/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1>ShopCo</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
